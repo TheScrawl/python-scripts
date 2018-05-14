126dc8f32b2b67b69ed0718189c88d60b691c0bd
TheScrawl <thescrawlgaming@gmail.com>
2018-05-14 10:37:24 +0000

    optimized box draw to be a bit better[A

--- ncurses-test.py
+++ ncurses-test.py
@@ -1,29 +1,21 @@
 import curses
 from curses import wrapper
 
-def drawMap(array, screen):
-	x = 1
-	y = 1
-	for c in array:
-		if c == 'newline':
-			y = y + 1
-			x = 1 
-		elif isinstance(c, str) and c != 'newline':
-			screen.addstr(y, x, c)
-			x = x + len(c)
-		elif isinstance(c, int):
-			x = x + c
-
+def drawMap(y, x, screen, array):
+	for i in array:
+		screen.addstr(y, x, i)
+		y = y + 1
+		
+			
 def main(window):
 	
-	maze = [u'──────────','newline', 
-			u'│', 8, u'│', 'newline',
-			u'│', 8, u'│', 'newline',
-			u'──────────']
+	maze = [u'┌────────┐', 
+			u'│        │',
+			u'│        │',
+			u'└────────┘']
 
 	playerY = 3
 	playerX = 3
-	borders = [u'─', u'━', u'│', u'┃', u'┄', u'┅', u'┆', u'┇', u'┈', u'┉', u'┊', u'┋', u'┌', u'┍', u'┎', u'┏',  u'┐', u'┑', u'┒', u'┓', u'└', u'┕', u'┖', u'┗', u'┘', u'┙', u'┚', u'┛', u'├', u'┝', u'┞', u'┟', u'┠', u'┡', u'┢', u'┣', u'┤', u'┥', u'┦', u'┧', u'┨', u'┩', u'┪', u'┫', u'┬', u'┭', u'┮', u'┯', u'┰', u'┱', u'┲', u'┳', u'┴', u'┵', u'┶', u'┷', u'┸', u'┹', u'┺', u'┻', u'┼', u'┽', u'┾', u'┿', u'╀', u'╁', u'╂', u'╃', u'╄', u'╅', u'╆', u'╇', u'╈', u'╉', u'╊', u'╋', u'╌', u'╍', u'╎', u'╏', u'═', u'║', u'╒', u'╓', u'╔', u'╕', u'╖', u'╗', u'╘', u'╙', u'╚', u'╛', u'╜', u'╝', u'╞', u'╟', u'╠', u'╡', u'╢', u'╣', u'╤', u'╥', u'╦', u'╧', u'╨', u'╩', u'╪', u'╫', u'╬', u'╭', u'╮', u'╯', u'╰', u'╱', u'╲', u'╳', u'╴', u'╵', u'╶', u'╷', u'╸', u'╹', u'╺', u'╻', u'╼', u'╽', u'╾', u'╿']
 
 	
 	#Setup Window
@@ -33,33 +25,33 @@ def main(window):
 	curses.curs_set(False)	
 	window.keypad(True)
 
-	drawMap(maze, window)
+	drawMap(1, 1, window, maze)
 	while True:
 		keypress = window.getch()
 		try:
 			if keypress == ord('w'):
-				if chr(window.inch(playerY - 1, playerX)) not in borders:
+				if chr(window.inch(playerY - 1, playerX)) == ' ':
 					window.addch(playerY, playerX, ' ')
 					window.addstr(playerY - 1, playerX, u'o')
 					playerY = playerY - 1
 				else: 
 					pass
 			if keypress == ord('s'):
-				if chr(window.inch(playerY + 1, playerX)) not in borders:
+				if chr(window.inch(playerY + 1, playerX)) == ' ':
 					window.addch(playerY, playerX, ' ')
 					window.addstr(playerY + 1, playerX, u'o')
 					playerY = playerY + 1	
 				else:
 					pass
 			if keypress == ord('a'):
-				if chr(window.inch(playerY, playerX - 1)) not in borders:
+				if chr(window.inch(playerY, playerX - 1)) == ' ':
 					window.addch(playerY, playerX, ' ')
 					window.addstr(playerY, playerX - 1, u'o')
 					playerX = playerX - 1			
 				else:
 					pass
 			if keypress == ord('d'):
-				if chr(window.inch(playerY, playerX + 1)) not in borders:
+				if chr(window.inch(playerY, playerX + 1)) == ' ':
 					window.addch(playerY, playerX, ' ')
 					window.addstr(playerY, playerX + 1, u'o')	
 					playerX = playerX + 1		
